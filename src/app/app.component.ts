28aa1ca95e1e18b1b6bd7bcb26f71f0ab8ca858b
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2019-10-18 09:55:25 +0900

    受信側の実装を@HostListnerに変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {fromEvent} from 'rxjs';
 import {filter, map} from 'rxjs/operators';
 
@@ -17,13 +17,23 @@ export class AppComponent {
     // 送信元のoriginは必ずチェックする。チェック対象の値はenvironmentに定義
     // event.dataの型をインターフェースとして合意する。
     // この処理まるごと共通サービス化したほうがいい。
-    fromEvent(window, 'message')
-      .pipe(
-        filter((event: MessageEvent) => event.origin === 'http://localhost:4201'),
-        map((event: MessageEvent) => event.data)
-      )
-      .subscribe( data => {
-        this.pulleMesage = data;
-      });
+    // fromEvent(window, 'message')
+    //   .pipe(
+    //     filter((event: MessageEvent) => event.origin === 'http://localhost:4201'),
+    //     map((event: MessageEvent) => event.data)
+    //   )
+    //   .subscribe( data => {
+    //     this.pulleMesage = data;
+    //   });
+  }
+  @HostListener('window:message', ['$event'])
+  getMessage(ev: MessageEvent) {
+    if (!this.agreedOrigin(ev)) { return; }
+
+    this.pulleMesage = ev.data;
+  }
+
+  private agreedOrigin(ev: MessageEvent) {
+    return ev.origin === 'http://localhost:4201';
   }
 }
